91a06ac97ff6edc3388a11a26519bfe18e6f7b39
Rezwan66 <shaikhrezwan66@gmail.com>
2023-10-23 02:22:42 +0600

    created basic layout for all pages routes, footer and navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer';
+import NavBar from '../pages/Shared/NavBar';
 
 const MainLayout = () => {
   return (
     <div>
+      <NavBar></NavBar>
+
       <Outlet></Outlet>
+
+      <Footer></Footer>
     </div>
   );
 };
