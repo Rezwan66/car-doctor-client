15be29c71321c8e50a5145f4da581375c093b271
Rezwan66 <shaikhrezwan66@gmail.com>
2023-10-26 19:39:02 +0600

    implemented delete a single booking by id from database

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
 import BookingRow from './BookingRow';
+import Swal from 'sweetalert2';
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
@@ -16,7 +17,37 @@ const Bookings = () => {
         console.log(data);
         setBookings(data);
       });
-  }, []);
+  }, [url]);
+
+  const handleDelete = _id => {
+    console.log(_id);
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then(result => {
+      if (result.isConfirmed) {
+        // Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+        fetch(`http://localhost:5000/bookings/${_id}`, {
+          method: 'DELETE',
+        })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire('Deleted!', 'Your file has been deleted.', 'success');
+              const remaining = bookings.filter(b => b._id !== _id);
+              setBookings(remaining);
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div>
       <h2 className="text-4xl font-semibold text-center">
@@ -28,22 +59,23 @@ const Bookings = () => {
             {/* head */}
             <thead>
               <tr>
-                <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
-                </th>
+                <th>Action</th>
                 <th>Service</th>
                 <th>Customer</th>
                 <th>Email</th>
                 <th>Date</th>
                 <th>Price</th>
+                <th>Status</th>
               </tr>
             </thead>
             <tbody>
               {/* row 1 */}
               {bookings?.map(booking => (
-                <BookingRow key={booking._id} booking={booking}></BookingRow>
+                <BookingRow
+                  key={booking._id}
+                  booking={booking}
+                  handleDelete={handleDelete}
+                ></BookingRow>
               ))}
             </tbody>
           </table>
